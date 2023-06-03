088a8dca5d90f9d6ec8573395cb9aa5588a5a105
Aser Hailu <aserhailu2020@gmail.com>
2023-06-03 13:21:48 +0300

    ui updated

--- client/src/components/teams/Team.jsx
+++ client/src/components/teams/Team.jsx
@@ -7,9 +7,14 @@ const DataContext = createContext();
 const DataProvider = ({ children }) => {
   const [data, setData] = useState([]);
 
+ 
+
   useEffect(() => {
     axios.get('https://gdsc-main-site.onrender.com/v1/member')
       .then(response => setData(response.data))
+      .then(response => {
+       
+      })
       .catch(error => console.log(error));
   }, []);
 
@@ -25,15 +30,23 @@ const DataProvider = ({ children }) => {
 
 const CardList = () => {
   const data = useContext(DataContext);
- 
-  const groupedData = data.reduce((acc, member) => {
-    const startYear = member.year_joined;
-    if (!acc[startYear]) {
-      acc[startYear] = [];
-    }
-    acc[startYear].push(member);
-    return acc;
-  }, {});
+  function groupBy(data, key) {
+    return data.reduce((result, item) => {
+      const group = item[key];
+      result[group] = result[group] || [];
+      result[group].push(item);
+      return result;
+    }, {});
+  }
+  
+  const groupedByYear = groupBy(data, 'year_joined');
+  const groupedByYearAndPosition = {};
+
+  Object.keys(groupedByYear).forEach(year => {
+    groupedByYearAndPosition[year] = groupBy(groupedByYear[year], 'position');
+  });
+
+
 
   return (
     <div className='container'>
@@ -43,24 +56,38 @@ const CardList = () => {
          amet numquam, deleniti iusto rerum velit nostrum aliquid eveniet sequi optio et fuga? Quod!</p>
      </div>
       <div >
-      {Object.entries(groupedData).map(([startYear, members]) => (
+      {Object.entries(groupedByYearAndPosition).map(([startYear, team]) => (
         <Accordion defaultActiveKey="0" className=''>
         <Accordion.Item  className='mt-3 mx-5 '>
         <Accordion.Header><span>{startYear}</span>/<span>{parseInt(startYear) + 1}  year</span></Accordion.Header>
           <Accordion.Body className=' core-team-member-card  flex flex-row flex-wrap'>
-            {
-              members.map((member) =>{
-                return(
-                  <div className='flex flex-col sm:flex-row'>
-                    <MemberCard key={member.id} member = { member }/>
-                  </div>
-                      )
-                  })
-                }
+          {
+                    
+                    Object.entries(team).map(([position,members]) =>(
+                       <div>
+                        <h4 className='text-left team-header'>{position}</h4>
+                        <div className='flex'>
+                         {
+                           members.map((member) => {
+                             return (
+                              <div className='flex '>
+                                      <MemberCard key={member.id} member = { member }/>
+                             </div>
+                             )
+                         })
+                         }
+                         </div>
+                       </div>
+                    ))
+                   }
+             
+            
+          
+           
+            
           </Accordion.Body>
         </Accordion.Item>
         </Accordion>
-     
       ))}
     </div>
     </div>
